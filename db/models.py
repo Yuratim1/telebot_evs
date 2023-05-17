50714ad854f4c2581096384ebd9543d9fdb60334
Yuratim1 <timoshenkov.y@gmail.com>
2023-05-18 01:31:44 +0300

    work laptop commit

---
+++ db/models.py
@@ -0,0 +1,8 @@
+from typing import List, Optional
+from sqlalchemy import ForeignKey, String, create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
